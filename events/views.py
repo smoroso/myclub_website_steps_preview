18aadb898d723bd46cb243ab4543f1e514b146d6
Stephane Moroso <stephane.moroso@gmail.com>
2023-12-06 17:19:57 +0100

    End of tutorial Wednesday #2

--- events/views.py
+++ events/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
 # Create your views here.
-def home(request):
-    return render(request, "home.html", {})
+def home(request, year, month):
+    month = month.capitalize()
+    # Convert month from name to number
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+
+    # Create a calendar
+    cal = HTMLCalendar().formatmonth(year, month_number)
+
+    # Get current year
+    now = datetime.now()
+    current_year = now.year
+
+    # Get current time
+    time = now.strftime("%I:%M:%S %p")
+
+    return render(request, "home.html", {
+        "year": year,
+        "month": month,
+        "month_number": month_number,
+        "cal": cal,
+        "current_year": current_year,
+        "time": time,
+    })
