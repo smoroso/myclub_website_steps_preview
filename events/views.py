203bf2bb5a13ffbf9c440642266741cf9d34617e
Stephane Moroso <stephane.moroso@gmail.com>
2023-12-13 19:59:54 +0100

    End of tutorial Wednesday #3

--- events/views.py
+++ events/views.py
@@ -4,7 +4,7 @@ from calendar import HTMLCalendar
 from datetime import datetime
 
 # Create your views here.
-def home(request, year, month):
+def home(request, year=datetime.now().year, month=datetime.now().strftime("%B")):
     month = month.capitalize()
     # Convert month from name to number
     month_number = list(calendar.month_name).index(month)
@@ -20,7 +20,7 @@ def home(request, year, month):
     # Get current time
     time = now.strftime("%I:%M:%S %p")
 
-    return render(request, "home.html", {
+    return render(request, "events/home.html", {
         "year": year,
         "month": month,
         "month_number": month_number,
