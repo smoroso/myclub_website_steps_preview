5448e8a72f6bedcc1c20652cdbfee289d81c7945
Stephane Moroso <stephane.moroso@gmail.com>
2023-12-13 20:37:12 +0100

    End of tutorial Wednesday #4

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Venue, MyClubUser, Event
 
-# Register your models here.
+admin.site.register(Venue)
+admin.site.register(MyClubUser)
+admin.site.register(Event)
