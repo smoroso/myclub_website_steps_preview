a30875a007f173de0dec42acc5acf0e6413c99c0
Stephane Moroso <stephane.moroso@gmail.com>
2023-12-14 10:31:49 +0100

    End of tutorial Wednesday #5

--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("<int:year>/<str:month>/", views.home, name="home")
+    path("<int:year>/<str:month>/", views.home, name="home"),
+    path("events", views.all_events, name="list-events"),
 ]
