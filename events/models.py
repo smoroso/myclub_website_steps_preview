fa8b1a31a0b0728f1888aa8718c6159847f235ff
Stephane Moroso <stephane.moroso@gmail.com>
2023-12-14 10:59:59 +0100

    End of tutorial Wednesday #6

--- events/models.py
+++ events/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Venue(models.Model):
     name = models.CharField('Venue Name', max_length=120)
     address = models.CharField(max_length=300)
     zip_code = models.CharField('Zip Code', max_length=15)
-    phone = models.CharField('Contact Phone', max_length=25)
-    web = models.URLField('Website Address')
-    email_address = models.EmailField('Email Adress')
+    phone = models.CharField('Contact Phone', max_length=25, blank=True)
+    web = models.URLField('Website Address', blank=True)
+    email_address = models.EmailField('Email Adress', blank=True)
 
     def __str__(self):
         return self.name
@@ -27,7 +28,7 @@ class Event(models.Model):
     event_date = models.DateTimeField('Event Date')
     venue = models.ForeignKey(Venue, blank=True, null=True, on_delete = models.CASCADE)
     # venue = models.CharField(max_length=120)
-    manager = models.CharField(max_length=60)
+    manager = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     description = models.TextField(blank=True)
     attendees = models.ManyToManyField(MyClubUser, blank=True)
 
