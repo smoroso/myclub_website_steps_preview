bbaacc581c9c54a43bec031e3dacac2f776106b9
Stephane Moroso <stephane.moroso@gmail.com>
2023-12-18 15:56:42 +0100

    End of tutorial Wednesday #17

--- events/views.py
+++ events/views.py
@@ -8,6 +8,52 @@ from .forms import VenueForm, EventForm
 from django.http import HttpResponse
 import csv
 
+from django.http import FileResponse
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+
+
+# Generate a PDF File Venue List
+def venue_pdf(request):
+    # Create Bytestream buffer
+    buf = io.BytesIO()
+    # Create a canvas
+    c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+    # Create a text object
+    textob = c.beginText()
+    textob.setTextOrigin(inch, inch)
+    textob.setFont("Helvetica", 14)
+
+    # Designate the Model
+    venues = Venue.objects.all()
+
+    # Create blank list
+    lines = []
+    # Loop
+    for venue in venues:
+        lines.append(venue.name)
+        lines.append(venue.address)
+        lines.append(venue.zip_code)
+        lines.append(venue.phone)
+        lines.append(venue.web)
+        lines.append(venue.email_address)
+        lines.append(" ")
+
+    # Loop
+    for line in lines:
+        textob.textLine(line)
+    
+    # Finish Up
+    c.drawText(textob)
+    c.showPage()
+    c.save()
+    buf.seek(0)
+
+    # Return something
+    return FileResponse(buf, as_attachment=True, filename="venue.pdf")
+
 
 # Generate CSV File Venue List
 def venue_csv(request):
