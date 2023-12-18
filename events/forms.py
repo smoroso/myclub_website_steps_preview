ee9c3eca206eb053a30ab8b1b7a6f642eeed39e9
Stephane Moroso <stephane.moroso@gmail.com>
2023-12-18 11:00:45 +0100

    End of tutorial Wednesday #11

--- events/forms.py
+++ events/forms.py
@@ -1,7 +1,29 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Venue
+from .models import Venue, Event
 
+
+class EventForm(ModelForm):
+    class Meta:
+        model = Event
+        # fields = "__all__"
+        fields = ("name", "event_date", "venue", "manager", "description", "attendees")
+        labels = {
+            "name": "Venue",
+            "event_date": "YYYY-MM-DD HH:MM:SS",
+            "venue": "Venue",
+            "manager": "Manager",
+            "description": "",
+            "attendees": "Attendees",
+        }
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control", "placeholder": "Event Name"}),
+            "event_date": forms.TextInput(attrs={"class": "form-control", "placeholder": "Event Date"}),
+            "venue": forms.Select(attrs={"class": "form-select", "placeholder": "Venue"}),
+            "manager": forms.Select(attrs={"class": "form-select", "placeholder": "Manager"}),
+            "description": forms.Textarea(attrs={"class": "form-control", "placeholder": "Description"}),
+            "attendees": forms.SelectMultiple(attrs={"class": "form-control", "placeholder": "Attendees"}),
+        }
 
 # Create a venue form
 class VenueForm(ModelForm):
