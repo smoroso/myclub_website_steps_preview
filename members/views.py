ca6b80eee330ab8f7cc98620c39312edabe8dcf1
Stephane Moroso <stephane.moroso@gmail.com>
2023-12-21 13:35:24 +0100

    End of tutorial Wednesday #25

--- members/views.py
+++ members/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
 def login_user(request):
     if request.method == "POST":
@@ -24,16 +25,16 @@ def logout_user(request):
 
 def register_user(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterUserForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data["username"]
             password = form.cleaned_data["password1"]
-            user = authenticate(suername=username, password=password)
+            user = authenticate(username=username, password=password)
             login(request, user)
             messages.success(request, ("Registration Successful"))
     else:
-        form = UserCreationForm()
+        form = RegisterUserForm()
 
     return render(request, 'authenticate/register_user.html', {
         "form": form,
